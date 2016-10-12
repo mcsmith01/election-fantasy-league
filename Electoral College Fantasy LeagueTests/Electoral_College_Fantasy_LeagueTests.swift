5a091ac71c209f70d3c029476994a1a1e906a209
Chase Smith <mcsmith01@alumni.amherst.edu>
2016-10-12 19:05:47 -0400

    Initial Commit

---
+++ Electoral College Fantasy LeagueTests/Electoral_College_Fantasy_LeagueTests.swift
@@ -0,0 +1,36 @@
+//
+//  Electoral_College_Fantasy_LeagueTests.swift
+//  Electoral College Fantasy LeagueTests
+//
+//  Created by Chase Smith on 10/12/16.
+//  Copyright © 2016 ls -applications. All rights reserved.
+//
+
+import XCTest
+@testable import Electoral_College_Fantasy_League
+
+class Electoral_College_Fantasy_LeagueTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
